54fe6a2779e991f329b8ce098eddc6d82047e590
Ali <32133661+Ghadiri1979@users.noreply.github.com>
2022-07-12 14:40:30 +0430

    Add files via upload

---
+++ 01 Hello World (JS).js
@@ -0,0 +1,12 @@
+// Much like python, in JavaScript (JS), you can print your output
+// using the print() function, such as in Line 4.
+
+// Notice that running the code does not print out what all the extra
+// stuff we wrote. '//' is how you comment out lines of code
+
+print('Hello World!')   // To run your script, press CTRL+ENTER
+
+
+
+
+
